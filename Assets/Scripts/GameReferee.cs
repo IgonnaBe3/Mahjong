e8a9f3d91d5b43b4abf2b00d8ae3670029891511
IgonnaBe3 <kacper.kolczuga@gmail.com>
2023-01-15 23:41:04 +0100

    Begininngs of Call system

--- Assets/Scripts/GameReferee.cs
+++ Assets/Scripts/GameReferee.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class GameReferee : MonoBehaviour
 {
     public Table table;
-    public Player CurrentPlayer; 
+    public Player CurrentPlayer;
+    //public Player PreviousPlayer;
+    Timer timer;
     public CallChecker CallChecker = new CallChecker();
+    float CallWaitTimeMax = 2;
     public void StartGame()
     {
+        timer = new Timer(CallWaitTimeMax * 1000);
+        timer.AutoReset = false;
+        timer.Elapsed += HandleCalls;
+
         CurrentPlayer = table.Players[0];
         for(int i = 0; i < 4; i++)
         {
@@ -23,25 +32,53 @@ public class GameReferee : MonoBehaviour
 
         if (nextIndex >= table.Players.Count)
             nextIndex = 0;
-
-        //if (CallChecker.CanChi(table.Players.Hand, LastDiscardedTile)) // Only prev player
-        //    Debug.Log($"Player {table.Players.IndexOf(CurrentPlayer)} can chi on {LastDiscardedTile.Value} of {LastDiscardedTile.Type}");
+        bool SomeoneHasCall=false;
+        if (CallChecker.CanChi(table.Players[nextIndex].Hand, LastDiscardedTile))
+        {
+            Debug.Log($"Player {table.Players.IndexOf(table.Players[nextIndex])} can chi on {LastDiscardedTile.Value} of {LastDiscardedTile.Type}");
+            SomeoneHasCall = true;
+        }
 
         foreach (Player player in table.Players)
         {
-            
-            CallChecker.CanPon(player.Hand, LastDiscardedTile);
-            CallChecker.CanKan(player.Hand, LastDiscardedTile);
+            if(CurrentPlayer != player)
+            {
+                if(CallChecker.CanPon(player.Hand, LastDiscardedTile))
+                {
+                    Debug.Log($"Player {table.Players.IndexOf(player)} can pon on {LastDiscardedTile.Value} of {LastDiscardedTile.Type}");
+                    SomeoneHasCall=true;
+                }
+
+                if(CallChecker.CanKan(player.Hand, LastDiscardedTile))
+                {
+                    Debug.Log($"Player {table.Players.IndexOf(player)} can kan on {LastDiscardedTile.Value} of {LastDiscardedTile.Type}");
+                    SomeoneHasCall = true;
+                }
+
+            }
+
             //canRon
             //canTsumo
         }
-
-        CurrentPlayer = table.Players[nextIndex];
-        MahjongTile NextTile = DrawTileFromWall(table.Wall);
-        if (NextTile != null)
-            CurrentPlayer.AddTileToHand(NextTile);
-        //else
+        if(SomeoneHasCall)
+        {
+            timer.Start();
+            Debug.Log("SOMEONE HAS CALLL BOIIIIS");
+        }
+        else
+        {
+            CurrentPlayer = table.Players[nextIndex];
+            MahjongTile NextTile = DrawTileFromWall(table.Wall);
+            if (NextTile != null)
+                CurrentPlayer.AddTileToHand(NextTile);
+            //else
             // koniec gry
+        }
+    }
+
+    private void HandleCalls(object sender, ElapsedEventArgs e)
+    {
+        Debug.Log("DUPAA");
     }
 
     public MahjongTile DrawTileFromWall(TileSet tileset)
